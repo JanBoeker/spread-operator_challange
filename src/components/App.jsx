841c85d9f6b15cdd780ec29f502388f3afda5a19
Jan Böker <naj_b@gmx.de>
2021-06-04 19:00:24 +0200

    solution with angelas input

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,36 +1,35 @@
 import React, {useState} from "react";
-import ListItem from "./ListItem";
+//import ListItem from "./ListItem";
 
 function App() {
 
-  const [item, setItem] = useState("");
-  const [list, addToList] = useState(["firstItem"]);
+  const [inputText, setInputText] = useState("");
+  const [items, addItem] = useState([]);
 
   function handleChange(event) {
-    setItem(event.target.value);
+    setInputText(event.target.value);
   }
 
   function handleClick(event) {
-    event.preventDefault();
+    //event.preventDefault();
 
-    const newItem = item;
-    setItem("");
-
-    addToList((prevValue) => {
+    addItem((prevValues) => {
       return (
-        [...prevValue, newItem]
+        [...prevValues, inputText]
       );
     });
+
+    setInputText("");
   }
 
-  function createList(arrayItem, index) {
-    return (
-      <ListItem 
-        key={index}
-        item={arrayItem} 
-      />
-    );
-  }
+  // function createList(arrayItem, index) {
+  //   return (
+  //     <ListItem 
+  //       key={index}
+  //       item={arrayItem} 
+  //     />
+  //   );
+  // }
 
   return (
     <div className="container">
@@ -38,14 +37,15 @@ function App() {
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input onChange={handleChange} type="text" value={item}/>
+        <input onChange={handleChange} type="text" value={inputText}/>
         <button>
           <span onClick={handleClick}>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          {list.map(createList)}  
+          {/* {items.map(createList)}   */}
+          {items.map((todoItem)=> <li>{todoItem}</li>)}
         </ul>
       </div>    
     </div>
