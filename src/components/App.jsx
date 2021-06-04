b53250f1d2f3502dfce8ae0aef1a4ff7f8a65107
Jan Böker <naj_b@gmx.de>
2021-06-04 18:38:48 +0200

    my first solution

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,53 @@
-import React from "react";
+import React, {useState} from "react";
+import ListItem from "./ListItem";
 
 function App() {
+
+  const [item, setItem] = useState("");
+  const [list, addToList] = useState(["firstItem"]);
+
+  function handleChange(event) {
+    setItem(event.target.value);
+  }
+
+  function handleClick(event) {
+    event.preventDefault();
+
+    const newItem = item;
+    setItem("");
+
+    addToList((prevValue) => {
+      return (
+        [...prevValue, newItem]
+      );
+    });
+  }
+
+  function createList(arrayItem, index) {
+    return (
+      <ListItem 
+        key={index}
+        item={arrayItem} 
+      />
+    );
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
       <div className="form">
-        <input type="text" />
+        <input onChange={handleChange} type="text" value={item}/>
         <button>
-          <span>Add</span>
+          <span onClick={handleClick}>Add</span>
         </button>
       </div>
       <div>
         <ul>
-          <li>A Item </li>
+          {list.map(createList)}  
         </ul>
-      </div>
+      </div>    
     </div>
   );
 }
